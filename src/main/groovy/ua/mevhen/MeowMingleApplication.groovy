a24e425a6c2992d94e926d2ffdfe477bc52ffef7
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-21 17:15:29 +0300

    BATMAN

---
+++ src/main/groovy/ua/mevhen/MeowMingleApplication.groovy
@@ -0,0 +1,13 @@
+package ua.mevhen
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MeowMingleApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MeowMingleApplication, args)
+	}
+
+}

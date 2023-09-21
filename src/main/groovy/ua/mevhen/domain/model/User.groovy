26baf1c66b8f09217f628323dc30e38421eef4b9
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-21 18:06:03 +0300

    feat: user model

---
+++ src/main/groovy/ua/mevhen/domain/model/User.groovy
@@ -0,0 +1,46 @@
+package ua.mevhen.domain.model
+
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+import jakarta.validation.constraints.NotNull
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.mongodb.core.mapping.DBRef
+import org.springframework.data.mongodb.core.mapping.Document
+
+import java.time.LocalDate
+
+@Document(collection = "user")
+@ToString
+@EqualsAndHashCode
+class User {
+
+    @Id
+    String id
+
+    @NotNull
+    String username
+
+    @NotNull
+    String email
+
+    @NotNull
+    String password
+
+    String status
+
+    String bio
+
+    @DBRef
+    Set<User> subscribers
+
+    @DBRef
+    Set<User> subscriptions
+
+    @CreatedDate
+    LocalDate created
+
+    @LastModifiedDate
+    LocalDate updated
+}

6fa634cfcbe3512ffaf6ee6d9507f10cd839f0bf
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-21 18:17:12 +0300

    feat: user registration form validations

--- src/main/groovy/ua/mevhen/domain/dto/UserRegistration.groovy
+++ src/main/groovy/ua/mevhen/domain/dto/UserRegistration.groovy
@@ -3,12 +3,19 @@ package ua.mevhen.domain.dto
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+
+import static ua.mevhen.constraints.DomainConstraints.USERNAME_MAX_SIZE
+import static ua.mevhen.constraints.DomainConstraints.USERNAME_MIN_SIZE
+import static ua.mevhen.constraints.DomainConstraints.USER_BIO_MAX_SIZE
+import static ua.mevhen.constraints.DomainConstraints.USER_PASSWORD_REGEXP
+import static ua.mevhen.constraints.DomainConstraints.USER_EMAIL_REGEXP
 
 record UserRegistration(
-        @NotNull @NotBlank String username,
-        @NotNull @NotBlank String email,
-        @NotNull @NotBlank @Pattern(regexp = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/) String password,
-        String bio
+        @NotNull @NotBlank @Size(min = USERNAME_MIN_SIZE, max = USERNAME_MAX_SIZE) String username,
+        @NotNull @NotBlank @Pattern(regexp = USER_EMAIL_REGEXP) String email,
+        @NotNull @NotBlank @Pattern(regexp = USER_PASSWORD_REGEXP) String password,
+        @Size(max = USER_BIO_MAX_SIZE) String bio
 ) {
 
 }

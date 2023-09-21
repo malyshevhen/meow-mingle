a17e9fdf3b3a5362b47c82bb6493d5bccb3950fc
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-21 20:51:05 +0300

    refactor: remove unused constraint

--- src/main/groovy/ua/mevhen/constraints/DomainConstraints.groovy
+++ src/main/groovy/ua/mevhen/constraints/DomainConstraints.groovy
@@ -5,5 +5,4 @@ class DomainConstraints {
     public static final def USER_EMAIL_REGEXP = /^[\w\.-]+@[\w\.-]+\.\w+$/
     public static final def USERNAME_MIN_SIZE = 3
     public static final def USERNAME_MAX_SIZE = 30
-    public static final def USER_BIO_MAX_SIZE = 1000
 }

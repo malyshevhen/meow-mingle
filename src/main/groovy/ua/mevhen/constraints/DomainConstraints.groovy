397d80303827e09c11c7f285adef47c56fc1eec9
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-22 00:20:50 +0300

    refactor: explicit types for constraints and msgs

--- src/main/groovy/ua/mevhen/constraints/DomainConstraints.groovy
+++ src/main/groovy/ua/mevhen/constraints/DomainConstraints.groovy
@@ -1,8 +1,8 @@
 package ua.mevhen.constraints
 
 class DomainConstraints {
-    public static final def USER_PASSWORD_REGEXP = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/
-    public static final def USER_EMAIL_REGEXP = /^[\w\.-]+@[\w\.-]+\.\w+$/
-    public static final def USERNAME_MIN_SIZE = 3
-    public static final def USERNAME_MAX_SIZE = 30
+    public static final String USER_PASSWORD_REGEXP = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/
+    public static final String USER_EMAIL_REGEXP = /^[\w\.-]+@[\w\.-]+\.\w+$/
+    public static final int USERNAME_MIN_SIZE = 3
+    public static final int USERNAME_MAX_SIZE = 30
 }

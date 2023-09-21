3576be3d2fbcaf3f9104d7a8c8b042bda31395f1
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-21 20:52:36 +0300

    feat: MongoRepository for User

---
+++ src/main/groovy/ua/mevhen/repository/UserRepository.groovy
@@ -0,0 +1,6 @@
+package ua.mevhen.repository
+
+import org.springframework.data.mongodb.repository.MongoRepository
+import ua.mevhen.domain.model.User
+
+interface UserRepository extends MongoRepository<User, String> {}

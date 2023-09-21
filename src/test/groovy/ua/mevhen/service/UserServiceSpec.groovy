eaa8f9daf49a41ea300eb46d88f82371523a20dc
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-22 00:24:05 +0300

    test: skeleton for UserServiceSpec

---
+++ src/test/groovy/ua/mevhen/service/UserServiceSpec.groovy
@@ -0,0 +1,11 @@
+package ua.mevhen.service
+
+class UserServiceSpec extends AbstractIntegrationSpec {
+
+    private final UserService userService
+
+    UserServiceSpec(UserService userService) {
+        this.userService = userService
+    }
+
+}

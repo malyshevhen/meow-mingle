2744a1436fb3ea6701ce89a7c7ba7ea79a593450
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-22 23:23:33 +0300

    test: move EnableMongoRepositories

--- src/main/groovy/ua/mevhen/MeowMingleApplication.groovy
+++ src/main/groovy/ua/mevhen/MeowMingleApplication.groovy
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
+@EnableMongoRepositories
 class MeowMingleApplication {
 
 	static void main(String[] args) {

f0898fede381d41a38b345d1d2e509d418f78121
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-22 23:21:27 +0300

    refactor: remove unused method

--- src/main/groovy/ua/mevhen/repository/UserRepository.groovy
+++ src/main/groovy/ua/mevhen/repository/UserRepository.groovy
@@ -3,8 +3,4 @@ package ua.mevhen.repository
 import org.springframework.data.mongodb.repository.MongoRepository
 import ua.mevhen.domain.model.User
 
-interface UserRepository extends MongoRepository<User, String> {
-
-    User updateUsernameById(String id, String username)
-
-}
+interface UserRepository extends MongoRepository<User, String> { }

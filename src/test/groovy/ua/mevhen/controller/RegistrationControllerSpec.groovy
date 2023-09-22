e60d0e8f78a924364f6bfc2603edb523d6175fae
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-22 23:23:11 +0300

    test: swap to springboot test

--- src/test/groovy/ua/mevhen/controller/RegistrationControllerSpec.groovy
+++ src/test/groovy/ua/mevhen/controller/RegistrationControllerSpec.groovy
@@ -2,7 +2,8 @@ package ua.mevhen.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
@@ -11,10 +12,12 @@ import ua.mevhen.domain.dto.UserRegistration
 import ua.mevhen.service.UserService
 
 import static org.mockito.Mockito.when
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@WebMvcTest([RegistrationController])
+@SpringBootTest(webEnvironment = MOCK)
+@AutoConfigureMockMvc
 class RegistrationControllerSpec extends Specification {
 
     @Autowired

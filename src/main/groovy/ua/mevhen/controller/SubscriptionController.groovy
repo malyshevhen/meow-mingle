6fa5afa9953905a279e3c260b4e7815c0aa82ad5
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-23 13:11:01 +0300

    feat: parameter validation

--- src/main/groovy/ua/mevhen/controller/SubscriptionController.groovy
+++ src/main/groovy/ua/mevhen/controller/SubscriptionController.groovy
@@ -1,12 +1,13 @@
 package ua.mevhen.controller
 
+import jakarta.validation.constraints.NotBlank
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import ua.mevhen.domain.dto.UserInfo
 import ua.mevhen.service.UserService
 
-@RestController('/api/user/')
+@RestController('/api/user')
 class SubscriptionController {
 
     private final UserService userService
@@ -16,7 +17,7 @@ class SubscriptionController {
     }
 
     @PostMapping('/subscribe/{id}')
-    UserInfo subscribe(@PathVariable('id') subId) {
+    UserInfo subscribe(@PathVariable('id') @NotBlank subId) {
         return null
     }
 }

f42241534313c5bcbbc985701f890a6e6bd222cd
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-23 13:05:43 +0300

    feat: SecurityConfig

---
+++ src/main/groovy/ua/mevhen/config/SecurityConfig.groovy
@@ -0,0 +1,24 @@
+package ua.mevhen.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers('/api/user/register').permitAll()
+                it.requestMatchers('/api/user/**').hasRole('USER')
+                it.anyRequest().authenticated()
+            }
+            .build()
+    }
+}

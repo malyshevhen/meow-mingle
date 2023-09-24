8923fc037750b49b7e37083a1644cf7374c80664
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-25 00:27:00 +0300

    style: remove explicit types

--- src/main/groovy/ua/mevhen/constraints/DomainConstraints.groovy
+++ src/main/groovy/ua/mevhen/constraints/DomainConstraints.groovy
@@ -1,7 +1,7 @@
 package ua.mevhen.constraints
 
 class DomainConstraints {
-    public static final String USER_PASSWORD_REGEXP = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/
-    public static final int USERNAME_MIN_SIZE = 3
-    public static final int USERNAME_MAX_SIZE = 30
+    public static final USER_PASSWORD_REGEXP = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/
+    public static final USERNAME_MIN_SIZE = 3
+    public static final USERNAME_MAX_SIZE = 30
 }

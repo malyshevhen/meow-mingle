c2784d2b7c34e4a1229c52ffa8fe654a61a6737f
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-25 00:42:32 +0300

    test: logging

--- src/main/groovy/ua/mevhen/controller/RegistrationController.groovy
+++ src/main/groovy/ua/mevhen/controller/RegistrationController.groovy
@@ -1,5 +1,6 @@
 package ua.mevhen.controller
 
+import groovy.util.logging.Slf4j
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -9,6 +10,7 @@ import ua.mevhen.service.UserService
 
 @RestController
 @RequestMapping('/api/user/register')
+@Slf4j
 class RegistrationController {
 
     private final UserService userService
@@ -20,7 +22,10 @@ class RegistrationController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     UserInfo register(@RequestBody @Valid UserRegistration regForm) {
-        return userService.save(regForm)
+        log.info("Received a registration request for username: ${ regForm.getUsername() }");
+        def userInfo = userService.save(regForm)
+        log.info("User registered successfully with username: ${ regForm.getUsername() }");
+        return userInfo
     }
 
 }

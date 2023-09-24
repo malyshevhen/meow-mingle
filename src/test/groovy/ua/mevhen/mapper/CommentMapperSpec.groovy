a4f0dfc91426363e862e9f7ae1b64dc3ef6596d4
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-24 13:46:48 +0300

    test: add tests
     - AbstractIntegrationSpec
     - CommentMapperSpec
     - PostMapperSpec
     - PostServiceIntegrationSpec
     - UserServiceIntegrationSpec

---
+++ src/test/groovy/ua/mevhen/mapper/CommentMapperSpec.groovy
@@ -0,0 +1,57 @@
+package ua.mevhen.mapper
+
+import org.bson.types.ObjectId
+import spock.lang.Specification
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import ua.mevhen.domain.dto.CommentRequest
+import ua.mevhen.domain.model.Comment
+import ua.mevhen.domain.model.User
+
+import java.time.LocalDate
+import java.time.Month
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE
+
+@SpringBootTest(webEnvironment = NONE)
+class CommentMapperSpec extends Specification {
+
+    @Autowired
+    private CommentMapper commentMapper
+
+    def "test toResponse method"() {
+        given:
+        def id = new ObjectId()
+        def userId = new ObjectId()
+
+        def comment = new Comment(
+            id: id,
+            author: new User(id: userId),
+            content: "Test Comment",
+            created: LocalDate.of(2022, Month.JANUARY, 1),
+            updated: LocalDate.of(2022, Month.FEBRUARY, 1)
+        )
+
+        when:
+        def response = commentMapper.toResponse(comment)
+
+        then:
+        response.id == id.toString()
+        response.authorId == userId.toString()
+        response.content == "Test Comment"
+        response.created == LocalDate.of(2022, Month.JANUARY, 1)
+        response.updated == LocalDate.of(2022, Month.FEBRUARY, 1)
+    }
+
+    def "test toComment method"() {
+        given:
+        def request = new CommentRequest(content: "Test Comment")
+
+        when:
+        def comment = commentMapper.toComment(request)
+
+        then:
+        comment.content == "Test Comment"
+    }
+}
+
--- src/test/groovy/ua/mevhen/service/UserServiceIntegrationSpec.groovy
+++ src/test/groovy/ua/mevhen/service/UserServiceIntegrationSpec.groovy
@@ -1,40 +1,20 @@
 package ua.mevhen.service
 
-
+import org.bson.types.ObjectId
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
-import org.testcontainers.containers.MongoDBContainer
-import org.testcontainers.spock.Testcontainers
-import spock.lang.Shared
-import spock.lang.Specification
 import ua.mevhen.domain.dto.UserRegistration
 import ua.mevhen.exceptions.UserNotFoundException
+import ua.mevhen.repository.UserRepository
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE
-import static org.testcontainers.utility.DockerImageName.parse
-
-@SpringBootTest(webEnvironment = NONE)
-@Testcontainers
-class UserServiceIntegrationSpec extends Specification {
-
-    @Shared
-    static def mongo = new MongoDBContainer(parse('mongo:6.0')).withExposedPorts(27017)
-
-    static {
-        mongo.start()
-    }
-
-    @DynamicPropertySource
-    static void setMongoDbProperties(DynamicPropertyRegistry registry) {
-        registry.add('spring.data.mongodb.uri', mongo::getReplicaSetUrl)
-    }
+class UserServiceIntegrationSpec extends AbstractIntegrationSpec {
 
     @Autowired
     UserService userService
 
-    def "test save and findById operations"() {
+    @Autowired
+    UserRepository userRepository
+
+    def "test save operations"() {
         given:
         def registration = new UserRegistration(
             username: 'testUser',
@@ -44,13 +24,19 @@ class UserServiceIntegrationSpec extends Specification {
 
         when:
         def savedUserInfo = userService.save(registration)
-        def foundUserInfo = userService.findById(savedUserInfo.id)
 
         then:
         savedUserInfo.username == 'testUser'
         savedUserInfo.id != null
         !savedUserInfo.id.isBlank()
-        foundUserInfo.username == 'testUser'
+
+        when:
+        def id = new ObjectId(savedUserInfo.id)
+        def user = userRepository.findById(id).get()
+
+        then:
+        user.role == 'USER'
+        user.created != null
 
         cleanup:
         userService.deleteById(savedUserInfo.id)
@@ -59,9 +45,9 @@ class UserServiceIntegrationSpec extends Specification {
     def "test updateUsername operation"() {
         given:
         def registration = new UserRegistration(
-            username: "testUser",
-            email: "test@example.com",
-            password: "password123"
+            username: 'testUser',
+            email: 'test@example.com',
+            password: 'password123'
         )
 
         def savedUserInfo = userService.save(registration)
@@ -82,9 +68,9 @@ class UserServiceIntegrationSpec extends Specification {
     def "test deleteById operation"() {
         given:
         def registration = new UserRegistration(
-            username: "testUser",
-            email: "test@example.com",
-            password: "password123"
+            username: 'testUser',
+            email: 'test@example.com',
+            password: 'password123'
         )
 
         def savedUserInfo = userService.save(registration)
@@ -95,6 +81,47 @@ class UserServiceIntegrationSpec extends Specification {
 
         then:
         thrown UserNotFoundException
+    }
+
+    def "test subscribe and unsubscribe operations"() {
+        given:
+        def registration1 = new UserRegistration(
+            username: 'testUser1',
+            email: 'test@example.com',
+            password: 'password123'
+        )
+
+        def registration2 = new UserRegistration(
+            username: 'testUser2',
+            email: 'test@example.com',
+            password: 'password123'
+        )
+
+        when:
+        def userInfo1 = userService.save(registration1)
+        def userInfo2 = userService.save(registration2)
+
+        then:
+        userInfo1.id != null
+        userInfo2.id != null
+
+        when:
+        userService.subscribe(userInfo1.username, userInfo2.id)
+        def user1 = userService.findByUsername(userInfo1.username)
+        def user2 = userService.findByUsername(userInfo2.username)
+
+        then:
+        user1.subscriptions.contains(user2)
+        user2.subscribers.contains(user1)
+
+        when:
+        userService.unsubscribe(userInfo1.username, userInfo2.id)
+        user1 = userService.findByUsername(userInfo1.username)
+        user2 = userService.findByUsername(userInfo2.username)
+
+        then:
+        !user1.subscriptions.contains(user2)
+        !user2.subscribers.contains(user1)
     }
 
 }

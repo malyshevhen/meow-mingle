a4dd9b8acd780ccf899989bf21464ea5625bbd8c
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-25 13:29:40 +0300

    feat: reaction operations logic

---
+++ src/main/groovy/ua/mevhen/service/reaction/RedisReactionWorker.groovy
@@ -0,0 +1,38 @@
+package ua.mevhen.service.reaction
+
+import groovy.util.logging.Slf4j
+import org.springframework.data.redis.core.RedisTemplate
+import ua.mevhen.config.properties.ReactionServiceProperties
+import ua.mevhen.domain.events.Reaction
+
+import java.time.Duration
+
+@Slf4j
+class RedisReactionWorker implements Runnable {
+
+    private final RedisTemplate<String, Reaction> reactionRedisTemplate
+    private final ReactionService reactionService
+    private final ReactionServiceProperties properties
+
+    RedisReactionWorker(
+        RedisTemplate<String, Reaction> reactionRedisTemplate,
+        ReactionService reactionService,
+        ReactionServiceProperties properties
+    ) {
+        this.reactionRedisTemplate = reactionRedisTemplate
+        this.reactionService = reactionService
+        this.properties = properties
+    }
+
+    @Override
+    void run() {
+        def reaction = reactionRedisTemplate.opsForList()
+            .rightPop(properties.keyName, Duration.ofMillis(properties.timeout))
+
+        if (reaction != null) {
+            log.info("Received reaction event: '$reaction'")
+            reactionService.manageReaction(reaction)
+        }
+    }
+
+}

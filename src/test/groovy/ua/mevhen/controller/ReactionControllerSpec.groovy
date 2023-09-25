3807ee0f815be429d08529a7cebbe8b624b35e4b
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-25 15:22:26 +0300

    test: ReactionControllerSpec

---
+++ src/test/groovy/ua/mevhen/controller/ReactionControllerSpec.groovy
@@ -0,0 +1,61 @@
+package ua.mevhen.controller
+
+import org.bson.types.ObjectId
+import org.spockframework.spring.SpringBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Retry
+import ua.mevhen.domain.model.User
+import ua.mevhen.service.AbstractIntegrationSpec
+import ua.mevhen.service.PostService
+import ua.mevhen.service.UserService
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@AutoConfigureMockMvc
+class ReactionControllerSpec extends AbstractIntegrationSpec {
+
+    private static USERNAME = 'John'
+    private static POST_ID = new ObjectId().toString()
+
+    @Autowired
+    MockMvc mockMvc
+
+    @SpringBean
+    PostService postService = Mock(PostService)
+
+    @SpringBean
+    UserService userService = Mock(UserService)
+
+    @Retry(count = 2)
+    @WithMockUser(username = 'John')
+    def "should add a like reaction"() {
+        when:
+        def result = mockMvc.perform(post("/api/reaction/like/{postId}", POST_ID))
+        userService.findByUsername(USERNAME) >> new User()
+        Thread.sleep(100)
+
+        then:
+        1 * postService.addLike(USERNAME, POST_ID)
+        0 * postService.removeLike(_, _)
+        result.andExpect(status().isOk())
+    }
+
+    @Retry(count = 2)
+    @WithMockUser(username = "John")
+    def "should remove a like reaction"() {
+        when:
+        def result = mockMvc.perform(post("/api/reaction/unlike/{postId}", POST_ID))
+        userService.findByUsername(USERNAME) >> new User()
+        Thread.sleep(100)
+
+        then:
+        1 * postService.removeLike(USERNAME, POST_ID)
+        0 * postService.addLike(_, _)
+        result.andExpect(status().isOk())
+    }
+
+}
